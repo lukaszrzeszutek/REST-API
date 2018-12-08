e00b58e033fd433e63c15199a4b3d79398436069
lukaszrzeszutek <lucas.rzeszut@gmail.com>
2018-12-08 18:42:00 +0100

    Update

--- models/Post.php
+++ models/Post.php
@@ -85,6 +85,7 @@ class Post {
 
     }
 
+    // Create Post
     public function create() {
       // Create query
       $query = 'INSERT INTO ' .
@@ -109,6 +110,48 @@ class Post {
         $stmt->bindParam(':body', $this->body);
         $stmt->bindParam(':author', $this->author);
         $stmt->bindParam(':category_id', $this->category_id);
+
+        // Execute query
+        if($stmt->execute()){
+          return true;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
+
+    // Update Post
+    public function update() {
+      // Create query
+      $query = 'UPDATE ' .
+       $this->table . '
+      SET
+        title = :title,
+        body = :body,
+        author = :author,
+        category_id = :category_id
+        WHERE
+        id = :id';
+        
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Clean data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // Bind data
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
 
         // Execute query
         if($stmt->execute()){
